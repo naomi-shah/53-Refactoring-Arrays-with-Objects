f83e829b8b14f52935929cc3aeb265e6a18ae197
Naomi Shah <37555446+naomi-shah@users.noreply.github.com>
2019-03-13 11:13:44 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,6 @@
+var names = ["Nayelly", "Afrida", "Ashley", "Sadman"];
+var school = ["RRG", "Cristo Rey", "RRG", "Uber"];
+var attendance = [true, false, true, true];
+var favSnack = ["PB&J", "chocolate", "lentil chips", "dried mango"];
+var photo = ["https://i.imgur.com/uz7SKR4.png", "https://i.imgur.com/hjvobVz.png", "https://i.imgur.com/cDaT0jk.png", "https://i.imgur.com/0SQH2hD.png"];
+    
